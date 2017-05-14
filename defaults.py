0212a2c463b27a2a80b1adcc9f3d1681a5817791
debidatta <debi.me@gmail.com>
2017-05-14 13:05:00 -0400

    Update defaults.py

--- defaults.py
+++ defaults.py
@@ -1,3 +1,13 @@
+# Paths
+# Fill this according to own setup
+BACKGROUND_DIR = ''
+BACKGROUND_GLOB_STRING = '*/*.png'
+POISSON_BLENDING_DIR = '' # if downloaded from S
+SELECTED_LIST_FILE = 'selected.txt'
+DISTRACTOR_LIST_FILE = 'neg_list.txt' 
+DISTRACTOR_DIR = ''
+DISTRACTOR_GLOB_STRING = '*.jpg'
+
 # Parameters for generator
 NUMBER_OF_WORKERS = 1#26
 BLENDING_LIST = ['gaussian','poisson', 'none', 'box', 'motion']
@@ -10,13 +20,6 @@ MAX_NO_OF_DISTRACTOR_OBJECTS = 4
 WIDTH = 640
 HEIGHT = 480
 MAX_ATTEMPTS_TO_SYNTHESIZE = 20
-BACKGROUND_DIR = '/usr1/debidatd/rgbd_bgs'
-BACKGROUND_GLOB_STRING = '*/*.png'
-POISSON_BLENDING_DIR = '/usr1/debidatd/pb'
-SELECTED_LIST_FILE = 'selected.txt'
-DISTRACTOR_LIST_FILE = 'neg_list.txt' 
-DISTRACTOR_DIR = '/usr1/debidatd/bigbird/raw_rgbd/'
-DISTRACTOR_GLOB_STRING = '*.jpg'
 
 # Parameters for objects in images
 MIN_SCALE = 0.15 # min scale for scale augmentation
