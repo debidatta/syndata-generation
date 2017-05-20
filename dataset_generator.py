38621270b6c756db12cc96178f5fbbbec809bbec
Debidatta Dwibedi <debi.me@gmail.com>
2017-05-20 12:15:26 -0400

    Added docstrings

--- dataset_generator.py
+++ dataset_generator.py
@@ -24,6 +24,14 @@ from collections import namedtuple
 Rectangle = namedtuple('Rectangle', 'xmin ymin xmax ymax')
 
 def randomAngle(kerneldim):
+    """Returns a random angle used to produce motion blurring
+
+    Args:
+        kerneldim (int): size of the kernel used in motion blurring
+
+    Returns:
+        int: Random angle
+    """ 
     kernelCenter = int(math.floor(kerneldim/2))
     numDistinctLines = kernelCenter * 4
     validLineAngles = np.linspace(0,180, numDistinctLines, endpoint = False)
@@ -31,6 +39,15 @@ def randomAngle(kerneldim):
     return int(validLineAngles[angleIdx])
 
 def LinearMotionBlur3C(img):
+    """Performs motion blur on an image with 3 channels. Used to simulate 
+       blurring caused due to motion of camera.
+
+    Args:
+        img(NumPy Array): Input image with 3 channels
+
+    Returns:
+        Image: Blurred image by applying a motion blur with random parameters
+    """
     lineLengths = [3,5,7,9]
     lineTypes = ["right", "left", "full"]
     lineLengthIdx = np.random.randint(0, len(lineLengths))
@@ -45,6 +62,16 @@ def LinearMotionBlur3C(img):
     return blurred_img
 
 def overlap(a, b):
+    '''Find if two bounding boxes are overlapping or not. This is determined by maximum allowed 
+       IOU between bounding boxes. If IOU is less than the max allowed IOU then bounding boxes 
+       don't overlap
+
+    Args:
+        a(Rectangle): Bounding box 1
+        b(Rectangle): Bounding box 2
+    Returns:
+        bool: True if boxes overlap else False
+    '''
     dx = min(a.xmax, b.xmax) - max(a.xmin, b.xmin)
     dy = min(a.ymax, b.ymax) - max(a.ymin, b.ymin)
     
@@ -52,29 +79,19 @@ def overlap(a, b):
         return True
     else:
         return False
- 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Create dataset with different augmentations")
-    parser.add_argument("root",
-      help="The root directory which contains the images and annotations.")
-    parser.add_argument("exp",
-      help="The directory where images and annotation lists will be created.")
-    parser.add_argument("--selected",
-      help="Keep only selected instances in the test dataset. Default is to keep all instances in the roo directory", action="store_true")
-    parser.add_argument("--scale",
-      help="Add scale augmentation.Default is to not add scale augmentation.", action="store_true")
-    parser.add_argument("--rotation",
-      help="Add rotation augmentation.Default is to not add rotation augmentation.", action="store_true")
-    parser.add_argument("--num",
-      help="Number of times each image will be in dataset", default=1, type=int)
-    parser.add_argument("--dontocclude",
-      help="Add objects without occlusion. Default is to produce occlusions", action="store_true")
-    parser.add_argument("--add_distractors",
-      help="Add distractors objects. Default is to not use distractors", action="store_true")
-    args = parser.parse_args()
-    return args
 
 def get_list_of_images(root_dir, N=1):
+    '''Gets the list of images of objects in the root directory. The expected format 
+       is root_dir/<object>/<image>.jpg. Adds an image as many times you want it to 
+       appear in dataset.
+
+    Args:
+        root_dir(string): Directory where images of objects are present
+        N(int): Number of times an image would appear in dataset. Each image should have
+                different data augmentation
+    Returns:
+        list: List of images(with paths) that will be put in the dataset
+    '''
     img_list = glob.glob(os.path.join(root_dir, '*/*.jpg'))
     img_list_f = []
     for i in xrange(N):
@@ -82,11 +99,28 @@ def get_list_of_images(root_dir, N=1):
     return img_list_f
 
 def get_mask_file(img_file):
-    # Write custom code for getting mask file
+    '''Takes an image file name and returns the corresponding mask file. The mask represents
+       pixels that belong to the object. Default implentation assumes mask file has same path 
+       as image file with different extension only. Write custom code for getting mask file here
+       if this is not the case.
+
+    Args:
+        img_file(string): Image name
+    Returns:
+        string: Correpsonding mask file path
+    '''
     mask_file = img_file.replace('.jpg','.pbm')
     return mask_file
 
 def get_labels(imgs):
+    '''Get list of labels/object names. Assumes the images in the root directory follow root_dir/<object>/<image>
+       structure. Directory name would be object name.
+
+    Args:
+        imgs(list): List of images being used for synthesis 
+    Returns:
+        list: List of labels/object names corresponding to each image
+    '''
     labels = []
     for img_file in imgs:
         label = img_file.split('/')[-2]
@@ -94,6 +128,13 @@ def get_labels(imgs):
     return labels
 
 def get_annotation_from_mask_file(mask_file, scale=1.0):
+    '''Given a mask file and scale, return the bounding box annotations
+
+    Args:
+        mask_file(string): Path of the mask file
+    Returns:
+        tuple: Bounding box annotation (xmin, xmax, ymin, ymax)
+    '''
     if os.path.exists(mask_file):
         mask = cv2.imread(mask_file)
         rows = np.any(mask, axis=1)
@@ -109,6 +150,13 @@ def get_annotation_from_mask_file(mask_file, scale=1.0):
         return -1, -1, -1, -1
 
 def get_annotation_from_mask(mask):
+    '''Given a mask, this returns the bounding box annotations
+
+    Args:
+        mask(NumPy Array): Array with the mask
+    Returns:
+        tuple: Bounding box annotation (xmin, xmax, ymin, ymax)
+    '''
     rows = np.any(mask, axis=1)
     cols = np.any(mask, axis=0)
     if len(np.where(rows)[0]) > 0:
@@ -119,17 +167,43 @@ def get_annotation_from_mask(mask):
         return -1, -1, -1, -1
 
 def write_imageset_file(exp_dir, img_files, anno_files):
+    '''Writes the imageset file which has the generated images and corresponding annotation files
+       for a given experiment
+
+    Args:
+        exp_dir(string): Experiment directory where all the generated images, annotation and imageset
+                         files will be stored
+        img_files(list): List of image files that were generated
+        anno_files(list): List of annotation files corresponding to each image file
+    '''
     with open(os.path.join(exp_dir,'train.txt'),'w') as f:
         for i in xrange(len(img_files)):
             f.write('%s %s\n'%(img_files[i], anno_files[i]))
 
 def write_labels_file(exp_dir, labels):
+    '''Writes the labels file which has the name of an object on each line
+
+    Args:
+        exp_dir(string): Experiment directory where all the generated images, annotation and imageset
+                         files will be stored
+        labels(list): List of labels. This will be useful while training an object detector
+    '''
     unique_labels = ['__background__'] + sorted(set(labels))
     with open(os.path.join(exp_dir,'labels.txt'),'w') as f:
         for i, label in enumerate(unique_labels):
             f.write('%s %s\n'%(i, label))
 
 def keep_selected_labels(img_files, labels):
+    '''Filters image files and labels to only retain those that are selected. Useful when one doesn't 
+       want all objects to be used for synthesis
+
+    Args:
+        img_files(list): List of images in the root directory
+        labels(list): List of labels corresponding to each image
+    Returns:
+        new_image_files(list): Selected list of images
+        new_labels(list): Selected list of labels corresponidng to each imahe in above list
+    '''
     with open(SELECTED_LIST_FILE) as f:
         selected_labels = [x.strip() for x in f.readlines()]
     new_img_files = []
@@ -141,17 +215,48 @@ def keep_selected_labels(img_files, labels):
     return new_img_files, new_labels
 
 def PIL2array1C(img):
+    '''Converts a PIL image to NumPy Array
+
+    Args:
+        img(PIL Image): Input PIL image
+    Returns:
+        NumPy Array: Converted image
+    '''
     return np.array(img.getdata(),
                     np.uint8).reshape(img.size[1], img.size[0])
 
 def PIL2array3C(img):
+    '''Converts a PIL image to NumPy Array
+
+    Args:
+        img(PIL Image): Input PIL image
+    Returns:
+        NumPy Array: Converted image
+    '''
     return np.array(img.getdata(),
                     np.uint8).reshape(img.size[1], img.size[0], 3)
 
 def create_image_anno_wrapper(args, w=WIDTH, h=HEIGHT, scale_augment=False, rotation_augment=False, blending_list=['none'], dontocclude=False):
+   ''' Wrapper used to pass params to workers
+   '''
    return create_image_anno(*args, w=w, h=h, scale_augment=scale_augment, rotation_augment=rotation_augment, blending_list=blending_list, dontocclude=dontocclude)
 
 def create_image_anno(objects, distractor_objects, img_file, anno_file, bg_file,  w=WIDTH, h=HEIGHT, scale_augment=False, rotation_augment=False, blending_list=['none'], dontocclude=False):
+    '''Add data augmentation, synthesizes images and generates annotations according to given parameters
+
+    Args:
+        objects(list): List of objects whose annotations are also important
+        distractor_objects(list): List of distractor objects that will be synthesized but whose annotations are not required
+        img_file(str): Image file name
+        anno_file(str): Annotation file name
+        bg_file(str): Background image path 
+        w(int): Width of synthesized image
+        h(int): Height of synthesized image
+        scale_augment(bool): Add scale data augmentation
+        rotation_augment(bool): Add rotation data augmentation
+        blending_list(list): List of blending modes to synthesize for each image
+        dontocclude(bool): Generate images with occlusion
+    '''
     if 'none' not in img_file:
         return 
     
@@ -272,6 +377,19 @@ def create_image_anno(objects, distractor_objects, img_file, anno_file, bg_file,
         f.write(xmlstr)
    
 def gen_syn_data(img_files, labels, img_dir, anno_dir, scale_augment, rotation_augment, dontocclude, add_distractors):
+    '''Creates list of objects and distrctor objects to be pasted on what images.
+       Spawns worker processes and generates images according to given params
+
+    Args:
+        img_files(list): List of image files
+        labels(list): List of labels for each image
+        img_dir(str): Directory where synthesized images will be stored
+        anno_dir(str): Directory where corresponding annotations will be stored
+        scale_augment(bool): Add scale data augmentation
+        rotation_augment(bool): Add rotation data augmentation
+        dontocclude(bool): Generate images with occlusion
+        add_distractors(bool): Add distractor objects whose annotations are not required 
+    '''
     w = WIDTH
     h = HEIGHT
     background_dir = BACKGROUND_DIR
@@ -331,9 +449,14 @@ def gen_syn_data(img_files, labels, img_dir, anno_dir, scale_augment, rotation_a
     return img_files, anno_files
 
 def init_worker():
+    '''
+    Catch Ctrl+C signal to termiante workers
+    '''
     signal.signal(signal.SIGINT, signal.SIG_IGN)
  
 def generate_synthetic_dataset(args):
+    ''' Generate synthetic dataset according to given args
+    '''
     img_files = get_list_of_images(args.root, args.num) 
     labels = get_labels(img_files)
 
@@ -354,6 +477,29 @@ def generate_synthetic_dataset(args):
     
     syn_img_files, anno_files = gen_syn_data(img_files, labels, img_dir, anno_dir, args.scale, args.rotation, args.dontocclude, args.add_distractors)
     write_imageset_file(args.exp, syn_img_files, anno_files)
+
+def parse_args():
+    '''Parse input arguments
+    '''
+    parser = argparse.ArgumentParser(description="Create dataset with different augmentations")
+    parser.add_argument("root",
+      help="The root directory which contains the images and annotations.")
+    parser.add_argument("exp",
+      help="The directory where images and annotation lists will be created.")
+    parser.add_argument("--selected",
+      help="Keep only selected instances in the test dataset. Default is to keep all instances in the roo directory", action="store_true")
+    parser.add_argument("--scale",
+      help="Add scale augmentation.Default is to not add scale augmentation.", action="store_true")
+    parser.add_argument("--rotation",
+      help="Add rotation augmentation.Default is to not add rotation augmentation.", action="store_true")
+    parser.add_argument("--num",
+      help="Number of times each image will be in dataset", default=1, type=int)
+    parser.add_argument("--dontocclude",
+      help="Add objects without occlusion. Default is to produce occlusions", action="store_true")
+    parser.add_argument("--add_distractors",
+      help="Add distractors objects. Default is to not use distractors", action="store_true")
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
     args = parse_args()
